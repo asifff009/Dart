18e22c1808b746929c55ff460b6c135423b4ebb1
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-07 23:22:55 +0600

    List demo..

---
+++ list.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  var number = [1,2,3,4,5];
+  var city = ['Dhaka','Rajshahi','Khulna'];
+  
+  // list is one kind of array..
+  
+  print(city);
+  print(number);
+  print(city[1]);
+}
