14a5c790e8603c212cee5ff20d0378be207e0adf
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-07 23:35:06 +0600

    Map uses

---
+++ 4_map.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  var person = {
+    'name' : 'Asif',
+    'age' : 21,
+    'city' : 'Mymensingh',
+    'gender' : 'Male'
+  };
+  
+  /*
+      List er motoi.. Just Map a hocche key r value
+      thake jeita list a naii... Map a first ta key, 
+      porer ta value..
+   
+  */
+
+  print(person);
+
+}
