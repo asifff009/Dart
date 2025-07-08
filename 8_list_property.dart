ac3026cf79a7b02c1589168742a84f74d2d37291
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-08 09:13:44 +0600

    Uses of List Property here

---
+++ 8_list_property.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var city = ['Dhaka', 'Khulna', 'Sylhet', 'Mymensingh'];
+
+  var r1 = city.length;
+  var r2 = city.first;
+  var r3 = city.last;
+  var r4 = city.reversed;
+  var r5 = city.isEmpty;
+
+
+  print(r1);
+  print(r2);
+  print(r3);
+  print(r4);
+  print(r5);
+  
+}
