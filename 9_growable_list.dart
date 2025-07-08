a17ad1cc42ab0835f28260768879b70f92f0929f
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-08 09:19:51 +0600

    Uses of Growable List here

---
+++ 9_growable_list.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  //Growable List -> jokon khushi add er maddhome add korte parbo..
+  
+  var city = ['Dhaka', 'Khulna', 'Sylhet', 'Mymensingh'];
+  print(city);
+
+  city.add("Bogura");
+  print(city);
+
+}
