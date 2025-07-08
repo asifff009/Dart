1e21ba4ab015ef59c29b648e7d013375a8d41f83
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-08 08:49:46 +0600

    Uses Unary Operation here

---
+++ 6_unary.dart
@@ -0,0 +1,16 @@
+// Unary Operation
+
+void main(){
+
+  var a = 10;
+
+  print(a++); //10
+  print(a++); //11
+  print(++a); //13
+  print(++a); //14
+  print(a--); //14
+  print(a--); //13
+  print(--a); //11
+  print(--a); //10
+  
+}
