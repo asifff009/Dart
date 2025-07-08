8a646af7c3a7eb83d79921461d02c6ec2d958144
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-08 08:58:33 +0600

    How constant works in dart

---
+++ 7_constant.dart
@@ -0,0 +1,18 @@
+/*
+          Constant in Dart
+          ----------------
+          |
+          ---->> 2 ta keyword diyee kora jay
+                  -> final
+                  -> const
+ */
+
+void main(){
+
+  const thisYear = 2025;
+  final thisMonth = 'July';
+
+  print(thisYear);
+  print(thisMonth);
+
+}
