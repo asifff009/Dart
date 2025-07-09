f82b204448e57d33fdb3b4a59cae876bf1e68df9
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-09 18:54:21 +0600

    uses of property of Sets in dart

---
+++ 2.15_set_properties.dart
@@ -0,0 +1,12 @@
+void main(){
+
+ var myCitySet = <String>{'Dhaka','Cumilla','Noakhali'};
+ print(myCitySet); // set a 1 ta element ekbar e print hoy. pore same element dilew print hobe na
+
+ print(myCitySet.first);
+ print(myCitySet.last);
+ print(myCitySet.length);
+ print(myCitySet.isEmpty);
+ print(myCitySet.isNotEmpty);
+ 
+}
