6581d903497a2a84f3529669c3694dacfc6b917d
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-09 18:49:20 +0600

    uses of sets in Dart

--- 2.14_set.dart
+++ 2.14_set.dart
@@ -9,4 +9,12 @@ void main(){
   myCitySet.addAll({'Rajhshahi','Rangpur','Jamalpur'});
   print(myCitySet);
 
+  print(myCitySet.elementAt(1)); // 1 no index er man ta print korbe
+ 
+ /*
+      myCitySet.clear();  --> ei command er maddhome shob 
+                              clear kore fela jay...
+  */
+  
+
 }
