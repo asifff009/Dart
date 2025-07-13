f3e493b912666c3e0f19ce276c59819b0e6c77c1
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-13 18:07:06 +0600

    Uses of Map Constructor and Property

---
+++ 2.16_map_constructor&properties.dart
@@ -0,0 +1,18 @@
+void main(){
+
+ var person = new Map();
+
+ person['name'] = 'Rahim Mia';
+ person['age'] = 40;
+ person['city'] = 'Dhaka';
+
+ print(person);
+
+ print(person.keys);
+ print(person.values);
+ print(person.length);
+ print(person.isEmpty);
+ print(person.isNotEmpty);
+
+
+}
