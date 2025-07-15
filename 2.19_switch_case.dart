347ef22ce05118a6086bffb0868245d94491f0b7
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-15 09:57:23 +0600

    uses of Switch Case

---
+++ 2.19_switch_case.dart
@@ -0,0 +1,22 @@
+void main(){
+
+ var marks = 80;
+
+ switch(marks){
+
+  case 90:
+   print("Brilliant");
+   break;
+
+  case 70:
+   print("Good");
+   break;
+
+  case 33:
+   print("Pass");
+   break;
+
+  default:
+   print("Result is not found");
+ }
+}
